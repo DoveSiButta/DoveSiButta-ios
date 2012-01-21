16fa5a381208beb480899eb5fbf160246a2d6334
Giovanni Maggini <giovanni.maggini@gmail.com>
2012-01-21 23:37:43 +0100

    First Commit

---
+++ DoveSiButta/Entities/RifiutoType.m
@@ -0,0 +1,13 @@
+//
+//  RifiutoType.m
+//  DoveSiButta
+//
+//  Created by Giovanni Maggini on 21/01/12.
+//  Copyright (c) 2012 Giovanni Maggini. All rights reserved.
+//
+
+#import "RifiutoType.h"
+
+@implementation RifiutoType
+
+@end
